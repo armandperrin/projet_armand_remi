49c468009ad356875c6b5e9c48fa7ffe1e0f5e5e
Rpollien <remi.pollien@saintmichelannecy.fr>
2024-10-29 15:41:20 +0100

    Ajout du fichier python de rémi

---
+++ test.py
@@ -0,0 +1,21 @@
+# content of test_sample.py
+def test_answer(cmdopt):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
+    assert 0  # to see what was printed
+
+# content of conftest.py
+
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+    )
+
+
+@pytest_addoption.fixture
+def cmdopt(request):
+    return request.config.getoption("--cmdopt")
