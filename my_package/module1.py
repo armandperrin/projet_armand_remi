2024c3472b110bfe2872b629977edf61d0e81e6d
armandperrin <armand.perrin@saintmichelannecy.fr>
2025-01-28 18:26:40 +0100

    Update module1.py

--- my_package/module1.py
+++ my_package/module1.py
@@ -1,1 +1,17 @@
-"# Module 1" 
+"""
+Module 1
+
+Ce module contient des fonctions liées aux messages de salutation.
+"""
+
+def greet(name: str) -> str:
+    """
+    Retourne un message de salutation.
+
+    Args:
+        name (str): Le nom de la personne.
+
+    Returns:
+        str: Un message de salutation.
+    """
+    return f"Bonjour, {name} !"
