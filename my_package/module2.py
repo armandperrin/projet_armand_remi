ce8e709679b2e04a88115fe1c6a65dceffc67c4c
armandperrin <armand.perrin@saintmichelannecy.fr>
2025-01-28 18:27:05 +0100

    Update module2.py

--- my_package/module2.py
+++ my_package/module2.py
@@ -1,1 +1,18 @@
-"# Module 2" 
+"""
+Module 2
+
+Ce module contient des fonctions mathématiques simples.
+"""
+
+def add_numbers(a: int, b: int) -> int:
+    """
+    Ajoute deux nombres entiers.
+
+    Args:
+        a (int): Le premier nombre.
+        b (int): Le second nombre.
+
+    Returns:
+        int: La somme des deux nombres.
+    """
+    return a + b
